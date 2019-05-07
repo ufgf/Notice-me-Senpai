b32ba8d6a961e90c86e1d0ed0a23df8c4ec201e4
ufgf <29230208+ufgf@users.noreply.github.com>
2019-05-07 21:42:17 +0300

    Add files via upload

--- trinity_auto.cmd
+++ trinity_auto.cmd
@@ -14,7 +14,7 @@ title Загрузка..  -  Trinity Auto
 :working
 	title Ввод ключа  -  Trinity Auto
 	set /p enteredkey="> Введите полученный ключ: "
-	psvimg-extract -K %enteredkey% game/game.psvimg game_dec| find "invalid key" > tmp.txt
+	psvimg-extract -K %enteredkey% game/game.psvimg game_dec 2>&1 | find "invalid key"  
 	pause>nul & cls & goto working
 
 
